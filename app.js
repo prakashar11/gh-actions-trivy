5e3447afb0648e64fd38a7661168c3d2c2a5a6d5
Abhay Bhargav <abhay@we45.com>
2020-11-20 17:59:27 +0530

    added new code + GH Actions

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const helmet = require("helmet");
 const app = express();
 
 // view engine setup
+// subjecting this to github actions
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
